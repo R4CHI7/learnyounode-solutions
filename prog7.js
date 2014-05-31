3376391b61052168bb2ccd7beb146c3a02a21c63
Rachit Gupta <rachitgupta1792@gmail.com>
2014-05-31 12:06:51 +0530

    Removed var string

--- prog7.js
+++ prog7.js
@@ -2,7 +2,7 @@ http = require('http');
 
 var URL = process.argv[2];
 
-var stream = http.get(URL, callback);
+http.get(URL, callback);
 
 function callback(response) {
 	response.setEncoding('utf-8');
