cd6823ade2fe502557a9d56396f8491d18dbc092
Rachit Gupta <rachitgupta1792@gmail.com>
2014-06-08 16:42:25 +0530

    Solved Problem 11: Http File Server

---
+++ prog11.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+
+var port = process.argv[2];
+var filename = process.argv[3];
+
+var server = http.createServer(function(request, response) {
+	response.writeHead(200, {'content-type' : 'text/plain'});
+	fs.createReadStream(filename).pipe(response);
+});
+
+server.listen(port);
